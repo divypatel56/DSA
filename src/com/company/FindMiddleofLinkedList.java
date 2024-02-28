8572c1dcc4ff9bd14eb21e9f420ee594fdb90441
Divy Patel <67093305+divypatel56@users.noreply.github.com>
2024-02-27 21:08:37 -0500

    Create FindMiddleofLinkedList.java

---
+++ src/com/company/FindMiddleofLinkedList.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slowptr = head;
+        ListNode fastptr = head;
+
+        // Travel until the fasst pointer reaches the last node or null
+        while(fastptr != null && fastptr.next !=null){
+            //Slow ptr moves 1 node
+            slowptr = slowptr.next;
+
+            //fast ptr moves 2 nodes
+            fastptr = fastptr.next.next;
+        }
+
+        return slowptr;
+        
+    }
+}
